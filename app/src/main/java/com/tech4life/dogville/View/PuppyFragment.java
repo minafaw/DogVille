4d905734cab8a80d764f9f0091df4bab2c274f5b
NT <NT2014>
2015-10-22 17:45:31 +0200

    add click listner

--- app/src/main/java/com/tech4life/dogville/View/PuppyFragment.java
+++ app/src/main/java/com/tech4life/dogville/View/PuppyFragment.java
@@ -1,19 +1,17 @@
 package com.tech4life.dogville.View;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.tech4life.dogville.Controller.DogsAdapter;
 import com.tech4life.dogville.Controller.Utils;
 import com.tech4life.dogville.Model.DogsModel;
 import com.tech4life.dogville.R;
@@ -21,7 +19,6 @@ import com.tech4life.dogville.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
 
 /**
  * Created by karma on 18/05/2015.
@@ -29,13 +26,13 @@ import java.util.Vector;
 
 public class PuppyFragment extends Fragment {
 
-    private Vector<DogsModel> puppyList = new Vector<>();
-    public static Handler operationHandler;
-    private Fragment fragment;
-    private FragmentTransaction fragmentTransaction;
-    static  ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-    private GridView gridView;
-    private TextView mTitle;
+    //private Vector<DogsModel> puppyList = new Vector<>();
+    //public static Handler operationHandler;
+    //private Fragment fragment;
+    //private FragmentTransaction fragmentTransaction;
+    static ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+    //private GridView gridView;
+    //private TextView mTitle;
     private String title = "Puppies";
 
     private List<DogsModel> _PuppyListItems = new ArrayList<>();
@@ -53,7 +50,13 @@ public class PuppyFragment extends Fragment {
         //getActivity().startService(intent);
         View rootView = inflater.inflate(R.layout.fragment_puppies, null);
 
-        _PuppyRecycleView = (RecyclerView)rootView.findViewById(R.id.puppyList);
+        _PuppyRecycleView = (RecyclerView) rootView.findViewById(R.id.puppyList);
+        _PuppyRecycleView.addOnItemTouchListener(new RecycleOntouchListner(getActivity(), new ClickListner() {
+            @Override
+            public void OnClick(View view, int position) {
+                Toast.makeText(getContext().getApplicationContext(), "position" + position , Toast.LENGTH_LONG).show();
+            }
+        }));
 
         //mTitle = (TextView) rootView.findViewById(R.id.mtitle);
         //gridView = (GridView) rootView.findViewById(R.id.gridView);
@@ -79,35 +82,80 @@ public class PuppyFragment extends Fragment {
         return rootView;
     }
 
-    private void setActionHandler() {
-        operationHandler = new Handler() {
-            @SuppressWarnings({"unchecked"})
-            public void handleMessage(Message msg) {
-                Utils.hideLoader();
-                {
-                    puppyList = ((Vector<DogsModel>) msg.obj);
-                    populateList();
+//    private void setActionHandler() {
+//        operationHandler = new Handler() {
+//            @SuppressWarnings({"unchecked"})
+//            public void handleMessage(Message msg) {
+//                Utils.hideLoader();
+//                {
+//                    puppyList = ((Vector<DogsModel>) msg.obj);
+//                    populateList();
+//                }
+//            }
+//        };
+//    }
+
+//    private void populateList() {
+//        list.clear();
+//
+//        DogsModel dogs[] = new DogsModel[puppyList.size()];
+//        for (int i = 0; i < puppyList.size(); i++) {
+//            DogsModel dog = (DogsModel) puppyList.elementAt(i);
+//            DogsModel element = new DogsModel();
+//            element.setImage(dog.getImage());
+//            element.setDogName(dog.getDogName());
+//            dogs[i] = element;
+//        }
+//
+//        DogsAdapter adapter = new DogsAdapter(getActivity(),
+//                R.layout.ctrl_listview_item_row, dogs);
+//        gridView.setAdapter(adapter);
+//
+//    }
+
+    public class RecycleOntouchListner implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector mGestureDetector;
+        private ClickListner mClickListner;
+
+        public RecycleOntouchListner(Context mContext, ClickListner clickListner) {
+
+            this.mClickListner = clickListner;
+            mGestureDetector = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+
+                    return true;
                 }
-            }
-        };
-    }
-
-    private void populateList() {
-        list.clear();
-
-        DogsModel dogs[] = new DogsModel[puppyList.size()];
-        for (int i = 0; i < puppyList.size(); i++) {
-            DogsModel dog = (DogsModel) puppyList.elementAt(i);
-            DogsModel element = new DogsModel();
-            element.setImage(dog.getImage());
-            element.setDogName(dog.getDogName());
-            dogs[i] = element;
+            });
         }
 
-        DogsAdapter adapter = new DogsAdapter(getActivity(),
-                R.layout.ctrl_listview_item_row, dogs);
-        gridView.setAdapter(adapter);
 
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && mClickListner != null && mGestureDetector.onTouchEvent(e)) {
+                mClickListner.OnClick(child, rv.getChildAdapterPosition(child));
+                return false;
+
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
+    public static interface ClickListner {
+        void OnClick(View view, int position);
     }
 
 }
